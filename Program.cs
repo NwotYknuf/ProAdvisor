f56db5c41654a4fa230c96ee49998037c8c22543
Nwot <jlafille@ensc.fr>
2019-11-14 14:53:24 +0100

    Ajouté un scrapper pour TrustPilot

--- Program.cs
+++ Program.cs
@@ -1,22 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace TrustedShopsCrawler {
     class Program {
         public static async Task Main(string[] args) {
 
-            TrustedShopsScrapper ts = new TrustedShopsScrapper();
-            List<Review> reviews = await ts.getReviews("moncoffrage.com");
+            string recherche = "www.pimkie.fr";
+            Console.WriteLine($"Recherche d'avis pour {recherche}");
+
+            TrustPilotScrapper tps = new TrustPilotScrapper();
+            List<Review> tp_reviews = await tps.getReviews(recherche);
+            Console.WriteLine($"{tp_reviews.Count} reviews trouvées pour TrustPilot");
+
+            TrustedShopsScrapper tss = new TrustedShopsScrapper();
+            List<Review> ts_reviews = await tss.getReviews(recherche);
+            Console.WriteLine($"{ts_reviews.Count} reviews trouvées pour TrustedShops");
 
             double moyenne = 0.0;
 
-            foreach (Review rev in reviews) {
+            List<Review> all_reviews = new List<Review>();
+            all_reviews.AddRange(tp_reviews);
+            all_reviews.AddRange(ts_reviews);
+
+            foreach (Review rev in all_reviews) {
                 moyenne += rev.note;
-                Console.WriteLine($"date : {rev.date}, note : {rev.note}, commentaire : \n{rev.commentaire}\n");
+                //Console.WriteLine($"date : {rev.date}, note : {rev.note}, commentaire : \n{rev.commentaire}\n");
             }
 
-            Console.WriteLine($"{reviews.Count} reviews trouvées, note moyenne : {moyenne/reviews.Count}");
+            string output = JsonConvert.SerializeObject(all_reviews);
+            StreamWriter sw = new StreamWriter("res.json");
+            sw.WriteLine(output);
+            sw.Close();
+
+            Console.WriteLine($"{all_reviews.Count} reviews trouvées au toal, note moyenne : {moyenne/all_reviews.Count}");
 
         }
     }
