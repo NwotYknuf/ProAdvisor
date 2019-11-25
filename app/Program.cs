4b1bdf9b26f70df2ee358a5a13a75e84579e5d72
Nwot <jlafille@ensc.fr>
2019-11-25 21:07:34 +0100

    Changé le main

--- app/Program.cs
+++ app/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -8,34 +10,55 @@ namespace ProAdvisor.app {
     class Program {
         public static void Main(string[] args) {
 
-            string recherche = "www.pimkie.fr";
-            Console.WriteLine($"Recherche d'avis pour {recherche}");
+            string recherche = "www.moncoffrage.com";
 
-            TrustPilotScrapper tps = new TrustPilotScrapper();
-            List<Review> tp_reviews = tps.getReviews(recherche).Result;
-            Console.WriteLine($"{tp_reviews.Count} reviews trouvées pour TrustPilot");
+            List<Bot> bots = new List<Bot>();
 
-            TrustedShopsScrapper tss = new TrustedShopsScrapper();
-            List<Review> ts_reviews = tss.getReviews(recherche).Result;
-            Console.WriteLine($"{ts_reviews.Count} reviews trouvées pour TrustedShops");
+            bots.Add(new TrustPilotScrapper());
+            bots.Add(new TrustedShopsScrapper());
 
-            double moyenne = 0.0;
+            ConcurrentDictionary<string, List<Review>> reviewsPerSource = new ConcurrentDictionary<string, List<Review>>();
+
+            Console.WriteLine($"Recherche d'avis pour : {recherche}");
+
+            /*
+             * Boucle en parallel pour chaque bot
+             */
+            Parallel.ForEach(
+                bots, new ParallelOptions { MaxDegreeOfParallelism = 4 },
+                (bot) => {
+                    try {
+                        List<Review> reviews = bot.getReviews(recherche).Result;
+                        reviewsPerSource.TryAdd(bot.source, reviews);
+                        Console.WriteLine($"{reviews.Count} reveiw(s) trouvée(s) pour la source {bot.source}");
+                    } catch (Exception e) {
+                        Console.WriteLine($"Erreur pour la source {bot.source} :\n{e.Message}");
+                    }
+                }
+            );
 
             List<Review> all_reviews = new List<Review>();
-            all_reviews.AddRange(tp_reviews);
-            all_reviews.AddRange(ts_reviews);
 
-            foreach (Review rev in all_reviews) {
-                moyenne += rev.note;
-                //Console.WriteLine($"date : {rev.date}, note : {rev.note}, commentaire : \n{rev.commentaire}\n");
+            foreach (var keyValuePair in reviewsPerSource) {
+                all_reviews.AddRange(keyValuePair.Value);
             }
 
-            string output = JsonConvert.SerializeObject(all_reviews);
-            StreamWriter sw = new StreamWriter("res.json");
-            sw.WriteLine(output);
-            sw.Close();
+            if (all_reviews.Count > 0) {
+                double moyenne = 0.0;
+                foreach (Review rev in all_reviews) {
+                    moyenne += rev.note;
+                }
+                moyenne /= all_reviews.Count;
 
-            Console.WriteLine($"{all_reviews.Count} reviews trouvées au toal, note moyenne : {moyenne/all_reviews.Count}");
+                string output = JsonConvert.SerializeObject(all_reviews);
+                StreamWriter sw = new StreamWriter("res.json");
+                sw.WriteLine(output);
+                sw.Close();
+
+                Console.WriteLine($"{all_reviews.Count} reviews trouvées au total, note moyenne : {moyenne}");
+            } else {
+                Console.WriteLine($"Aucuns avis trouvés pour {recherche}");
+            }
 
         }
     }
