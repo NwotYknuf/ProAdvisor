6b8091998f0fa066a390a92bc504ca930ab3b0a6
Nwot <jlafille@ensc.fr>
2019-11-27 18:05:36 +0100

    Modification de review

--- app/Review.cs
+++ app/Review.cs
@@ -5,12 +5,12 @@ namespace ProAdvisor.app {
         //TODO ajouter auteur et source du commentaire
 
         public string commentaire { get; set; }
-        public string auteur { get; set; }
+        public Utilisateur auteur { get; set; }
         public double note { get; set; }
         public string source { get; set; }
         public DateTime date { get; set; }
 
-        public Review(DateTime date, string commentaire, double note, string source, string auteur) {
+        public Review(DateTime date, string commentaire, double note, string source, Utilisateur auteur) {
             this.date = date;
             this.commentaire = commentaire;
             this.note = note;
