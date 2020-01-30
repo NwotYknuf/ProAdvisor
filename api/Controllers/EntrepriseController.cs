45ac630fbe56e5c88d101d0f0ee3e2c27e7b9b6f
Nwot <jlafille@ensc.fr>
2020-01-30 12:21:06 +0100

    Retiré api de la route

--- api/Controllers/EntrepriseController.cs
+++ api/Controllers/EntrepriseController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class EntrepriseController : ControllerBase {
         private readonly ProAdvisorContext _context;
